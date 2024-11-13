0f663a63e6bf115415b91c4c5438c81f67e9e3cc
Edi <146419637+Ediasecas@users.noreply.github.com>
2024-11-13 12:56:38 +0100

    servltt

---
+++ MiAppServlet/src/main/java/com/ServletP3/MiServlet.java
@@ -0,0 +1,17 @@
+package com.ServletP3;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mi-servlet")
+public class MiServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>¡Hola desde el Servlet!</h1>");
+    }
+}
